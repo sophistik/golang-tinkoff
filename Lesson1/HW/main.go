e12374ca0d83b5d175327e2f2cd2807ce3486b24
sophistik <sophistik@users.noreply.github.com>
2020-02-27 16:37:42 +0300

    Add files via upload

--- Lesson1/HW/main.go
+++ Lesson1/HW/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"encoding/csv"
 	"fmt"
+	"github.com/pkg/errors"
 	"io"
 	"log"
 	"os"
@@ -25,48 +26,72 @@ type profitInfo struct {
 }
 
 func main() {
-	tickers := []string{"AAPL", "SBER", "AMZN"}
-
-	candles := getCandlesInfo("candles_5m.csv")
-	maxProfit := calculateMaxProfit(tickers, candles)
-	usersID, usersProfit := calculateUsersProfit("user_trades.csv")
-
-	writeData(usersID, tickers, maxProfit, usersProfit, "output.csv")
-}
-
-func getCandlesInfo(inputFile string) map[string][]CandleInfo {
-	candles := map[string][]CandleInfo{
-		"AAPL": {},
-		"SBER": {},
-		"AMZN": {},
+	candles, err := getCandlesInfo("candles_5m.csv")
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	csvCandles, candlesError := os.Open(inputFile)
+	maxProfit := calculateMaxProfit(candles)
 
-	checkError("Error while opening \"candles_5m.csv\"", candlesError)
+	usersID, usersProfit, err := calculateUsersProfit("user_trades.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = writeData(usersID, maxProfit, usersProfit, "output.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getCandlesInfo(inputFile string) (map[string][]CandleInfo, error) {
+	candles := make(map[string][]CandleInfo)
+
+	csvCandles, err := os.Open(inputFile)
+	if err != nil {
+		return nil, errors.Wrap(err, "can't open file")
+	}
 
 	defer csvCandles.Close()
 
-	candlesReader := csv.NewReader(bufio.NewReader(csvCandles))
+	candlesInput := csv.NewReader(bufio.NewReader(csvCandles))
 
 	for {
-		line, candlesReaderError := candlesReader.Read()
-		if candlesReaderError == io.EOF {
-			break
-		} else if candlesReaderError != nil {
-			log.Fatal(candlesReaderError)
+		line, err := candlesInput.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			return nil, errors.Wrap(err, "error while reading file")
 		}
 
 		ticker := line[0]
-		opened, openedParceError := strconv.ParseFloat(line[2], 64)
-		higher, higherParceError := strconv.ParseFloat(line[3], 64)
-		lower, lowerParceError := strconv.ParseFloat(line[4], 64)
-		closed, closedParceError := strconv.ParseFloat(line[5], 64)
 
-		checkError("Error while parcing", openedParceError)
-		checkError("Error while parcing", higherParceError)
-		checkError("Error while parcing", lowerParceError)
-		checkError("Error while parcing", closedParceError)
+		opened, err := strconv.ParseFloat(line[2], 64)
+		if err != nil {
+			return nil, errors.Wrap(err, "error while parcing opened variable")
+		}
+
+		higher, err := strconv.ParseFloat(line[3], 64)
+		if err != nil {
+			return nil, errors.Wrap(err, "error while parcing higher variable")
+		}
+
+		lower, err := strconv.ParseFloat(line[4], 64)
+		if err != nil {
+			return nil, errors.Wrap(err, "error while parcing lower variaible")
+		}
+
+		closed, err := strconv.ParseFloat(line[5], 64)
+		if err != nil {
+			return nil, errors.Wrap(err, "error while parcing closed variaible")
+		}
+
+		_, ok := candles[ticker]
+		if !ok {
+			candles[ticker] = make([]CandleInfo, 0)
+		}
 
 		candles[ticker] = append(candles[ticker], CandleInfo{
 			time:   line[1],
@@ -77,21 +102,17 @@ func getCandlesInfo(inputFile string) map[string][]CandleInfo {
 		})
 	}
 
-	return candles
+	return candles, nil
 }
 
-func calculateMaxProfit(tickers []string, candles map[string][]CandleInfo) map[string]profitInfo {
-	maxProfit := map[string]profitInfo{
-		"AAPL": {profit: 0, timeToBuy: "", timeToSell: ""},
-		"SBER": {profit: 0, timeToBuy: "", timeToSell: ""},
-		"AMZN": {profit: 0, timeToBuy: "", timeToSell: ""},
-	}
+func calculateMaxProfit(candles map[string][]CandleInfo) map[string]profitInfo {
+	maxProfit := make(map[string]profitInfo)
 
-	for _, ticker := range tickers {
-		candle := candles[ticker]
+	for ticker, candle := range candles {
 		for i := 0; i < len(candle); i++ {
 			for j := i; j < len(candle); j++ {
-				if candle[j].higher-candle[i].lower > maxProfit[ticker].profit {
+				_, ok := maxProfit[ticker]
+				if !ok || candle[j].higher-candle[i].lower > maxProfit[ticker].profit {
 					maxProfit[ticker] = profitInfo{
 						profit:     candle[j].higher - candle[i].lower,
 						timeToBuy:  candle[i].time,
@@ -105,33 +126,45 @@ func calculateMaxProfit(tickers []string, candles map[string][]CandleInfo) map[s
 	return maxProfit
 }
 
-func calculateUsersProfit(inputFile string) ([]int64, map[int64]map[string]float64) {
+func calculateUsersProfit(inputFile string) ([]int64, map[int64]map[string]float64, error) {
 	var usersID []int64
 
 	usersProfit := make(map[int64]map[string]float64)
 
-	csvTrades, tradesError := os.Open(inputFile)
-	checkError("Error while opening file", tradesError)
+	csvTrades, err := os.Open(inputFile)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "can't open file")
+	}
 
 	defer csvTrades.Close()
 	tradesReader := csv.NewReader(bufio.NewReader(csvTrades))
 
 	for {
-		line, tradesReaderError := tradesReader.Read()
-		if tradesReaderError == io.EOF {
-			break
+		line, err := tradesReader.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			return nil, nil, errors.Wrap(err, "error while reading file")
 		}
 
-		checkError("Error while reading file", tradesReaderError)
+		id, err := strconv.ParseInt(line[0], 10, 64)
+		if err != nil {
+			return nil, nil, errors.Wrap(err, "error while parcing id variaible")
+		}
 
-		id, idParceError := strconv.ParseInt(line[0], 10, 64)
 		ticker := line[2]
-		buyPrice, buyPriceParceError := strconv.ParseFloat(line[3], 64)
-		sellPrice, sellPriceParceError := strconv.ParseFloat(line[4], 64)
 
-		checkError("Error while parcing", idParceError)
-		checkError("Error while parcing", buyPriceParceError)
-		checkError("Error while parcing", sellPriceParceError)
+		buyPrice, err := strconv.ParseFloat(line[3], 64)
+		if err != nil {
+			return nil, nil, errors.Wrap(err, "error while parcing ticker variable")
+		}
+
+		sellPrice, err := strconv.ParseFloat(line[4], 64)
+		if err != nil {
+			return nil, nil, errors.Wrap(err, "error while parcing sellPrice variable")
+		}
 
 		_, ok := usersProfit[id]
 		if !ok {
@@ -147,20 +180,22 @@ func calculateUsersProfit(inputFile string) ([]int64, map[int64]map[string]float
 		usersProfit[id][ticker] += sellPrice - buyPrice
 	}
 
-	return usersID, usersProfit
+	return usersID, usersProfit, nil
 }
 
-func writeData(usersID []int64, tickers []string, maxProfit map[string]profitInfo, usersProfit map[int64]map[string]float64, outputFile string) {
-	output, outputError := os.Create(outputFile)
-	checkError("Error while creating file", outputError)
+func writeData(usersID []int64, maxProfit map[string]profitInfo, usersProfit map[int64]map[string]float64, outputFile string) error {
+	output, err := os.Create(outputFile)
+	if err != nil {
+		return errors.Wrap(err, "can't create file")
+	}
 
 	defer output.Close()
-	writer := csv.NewWriter(output)
 
-	// fmt.Println(usersProfit)
+	writer := csv.NewWriter(output)
+	defer writer.Flush()
 
 	for _, id := range usersID {
-		for _, ticker := range tickers {
+		for ticker := range maxProfit {
 			_, ok := usersProfit[id][ticker]
 			if ok {
 				diff := maxProfit[ticker].profit - usersProfit[id][ticker]
@@ -172,15 +207,13 @@ func writeData(usersID []int64, tickers []string, maxProfit map[string]profitInf
 					maxProfit[ticker].timeToSell,
 					maxProfit[ticker].timeToBuy}
 
-				writerError := writer.Write(data)
-				checkError("Error while writing data", writerError)
+				err := writer.Write(data)
+				if err != nil {
+					return errors.Wrap(err, "error while writing data")
+				}
 			}
 		}
 	}
-}
 
-func checkError(message string, err error) {
-	if err != nil {
-		log.Fatal(message, err)
-	}
+	return nil
 }
